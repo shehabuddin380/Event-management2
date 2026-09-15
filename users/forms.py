87a16ad0aa29ec2f07a0a3358181eb65863ca926
Shihab Ahmed <shehabuddin380@gmail.com>
2026-09-15 23:08:00 +0600

    update forms

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,20 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
-class CustomUserCreationForm(UserCreationForm):
-    class Meta:
-        model = CustomUser
-        fields = ("username", "email", "phone_number", "profile_picture")
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    phone = forms.CharField(required=False)
+    profile_picture = forms.ImageField(required=False)
 
-class CustomUserChangeForm(UserChangeForm):
     class Meta:
-        model = CustomUser
-        fields = ("first_name", "last_name", "email", "phone_number", "profile_picture")
+        model = User
+        fields = ['username', 'email', 'phone', 'profile_picture', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500',
+                'placeholder': field.label,
+            })
