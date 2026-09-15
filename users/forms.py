bf14adde1cc45e47e975989ea57f562a286f6746
Shihab Ahmed <shehabuddin380@gmail.com>
2026-09-15 23:15:47 +0600

    update forms and class

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
@@ -10,6 +10,25 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'phone', 'profile_picture', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500',
+                'placeholder': field.label,
+            })
+
+
+class CustomUserChangeForm(UserChangeForm):
+    password = None  # password field edit form এ দেখাতে চাই না
+    email = forms.EmailField(required=True)
+    phone = forms.CharField(required=False)
+    profile_picture = forms.ImageField(required=False)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'phone', 'profile_picture']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
