eb5ca36c45aa2850d5f99413892a83c91a7c9ac9
Shihab Ahmed <shehabuddin380@gmail.com>
2026-09-18 23:40:15 +0600

    update user forms

--- users/forms.py
+++ users/forms.py
@@ -9,10 +9,20 @@ from django.contrib.auth.models import User
 INPUT_CLASS = "w-full border border-slate-300 rounded px-3 py-2 text-slate-800 placeholder-slate-400 focus:outline-none focus:ring-2 focus:ring-amber-400 focus:border-amber-400 transition-colors"
 
 
+def style_fields(form_instance):
+    """Add theme classes + a safe placeholder (never 'None') to every field."""
+    for field_name, field in form_instance.fields.items():
+        placeholder = field.label or field_name.replace('_', ' ').title()
+        field.widget.attrs.update({
+            'class': INPUT_CLASS,
+            'placeholder': placeholder,
+        })
+
+
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    phone = forms.CharField(required=False)
-    profile_picture = forms.ImageField(required=False)
+    phone = forms.CharField(required=False, label='Phone')
+    profile_picture = forms.ImageField(required=False, label='Profile picture')
 
     class Meta:
         model = User
@@ -20,18 +30,14 @@ class CustomUserCreationForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for field in self.fields.values():
-            field.widget.attrs.update({
-                'class': INPUT_CLASS,
-                'placeholder': field.label,
-            })
+        style_fields(self)
 
 
 class CustomUserChangeForm(UserChangeForm):
     password = None  # edit form-e password field dekhate chai na
     email = forms.EmailField(required=True)
-    phone = forms.CharField(required=False)
-    profile_picture = forms.ImageField(required=False)
+    phone = forms.CharField(required=False, label='Phone')
+    profile_picture = forms.ImageField(required=False, label='Profile picture')
 
     class Meta:
         model = User
@@ -39,18 +45,10 @@ class CustomUserChangeForm(UserChangeForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for field in self.fields.values():
-            field.widget.attrs.update({
-                'class': INPUT_CLASS,
-                'placeholder': field.label,
-            })
+        style_fields(self)
 
 
 class CustomAuthenticationForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for field in self.fields.values():
-            field.widget.attrs.update({
-                'class': INPUT_CLASS,
-                'placeholder': field.label,
-            })
+        style_fields(self)
